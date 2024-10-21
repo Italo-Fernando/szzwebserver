a3022275ce8af8cb2688396c62662cdb7b9c39b5
Felipe Cardoso <rockett370@gmail.com>
2024-10-21 18:04:39 -0300

    feat: Change usercount values in load tests

--- test/run_all_tests.sh
+++ test/run_all_tests.sh
@@ -2,7 +2,7 @@ RESET_SERVER=${1:-false}
 HOST=${2:-localhost}
 sh clean_test_result.sh
 for variant in "B_SZZ" "R_SZZ"; do
-    for usercount in 10 20 30; do
+    for usercount in 1 20; do
         for run in $(seq 1 30); do
             if test "${RESET_SERVER}" = true; then
                 sh reset_server.sh
