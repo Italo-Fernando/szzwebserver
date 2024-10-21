e713f3553088d84a84c434c37ca8a0ec8ec9f77c
Felipe Cardoso <rockett370@gmail.com>
2024-10-20 22:19:35 -0300

    feat: Parameterize test runs

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,1 +1,2 @@
-jmeter -n -t ./SzzWebServer.jmx -l report.csv -e -o ./reports/ -q ./jmeter.properties
+RUN="${1:-0}"
+jmeter -n -t ./SzzWebServer.jmx -l report_${RUN}.csv -e -o ./reports_${RUN}/ -q ./jmeter.properties
