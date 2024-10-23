d264ade4cc74e903b0d1e70f3ff8ee317c7f14d0
Felipe Cardoso <rockett370@gmail.com>
2024-10-23 10:07:41 -0300

    refactor: Update script to generate csv from experiments

--- test/build_csv.sh
+++ test/build_csv.sh
@@ -6,7 +6,7 @@ x_num=1
 for variant in "B_SZZ" "R_SZZ"; do
     for rate in 1 20; do
         for run in $(seq 1 30); do  
-            m_dir="./$variant/rate_$rate"
+            m_dir="./results/$variant/rate_$rate"
             tempo_resposta=$(grep -i meanrestime "${m_dir}/report_${variant}_rate_${rate}_run_${run}_stats.json" | head -n 1 | awk '{print $3}'| tr -d ',')
             tempo_download=$(cat "${m_dir}/download_${variant}_rate_${rate}_run_${run}.txt")
             tempo_total=$(cat "${m_dir}/totaltime_${variant}_rate_${rate}_run_${run}.txt")
