f7bcd2e9900dea8b69b99447cbbadf636c978e97
Felipe Cardoso <rockett370@gmail.com>
2024-10-24 18:04:26 -0300

    feat: Change db tables to reference szz_variant foreign key

--- modules/database/init.sql
+++ modules/database/init.sql
@@ -1,9 +1,14 @@
 CREATE TYPE req_status as ENUM ('WAITING', 'PROCESSING', 'FINISHED', 'ERROR');
 
+CREATE TABLE public.szz_variant (
+    variant_name character varying(20),
+    PRIMARY KEY (variant_name)
+);
+
 CREATE TABLE public.execution_result (
     bugfix_commit_hash text NOT NULL,
     repository_url text NOT NULL,
-    szz_variant text NOT NULL,
+    szz_variant text REFERENCES szz_variant NOT NULL,
     bug_commit_hashes text[],
     request_status req_status NOT NULL DEFAULT 'WAITING',
     PRIMARY KEY (repository_url, bugfix_commit_hash, szz_variant)
@@ -12,7 +17,7 @@ CREATE TABLE public.execution_result (
 CREATE TABLE public.request (
     request_id bigint PRIMARY KEY,
     repository_url text NOT NULL,
-    szz_variant text NOT NULL,
+    szz_variant text REFERENCES szz_variant NOT NULL,
     bugfix_commit_hashes text[] NOT NULL,
     commit_count integer NOT NULL,
     request_status req_status NOT NULL DEFAULT 'WAITING',
@@ -34,13 +39,8 @@ CREATE TABLE public.commit_to_request_link (
     request_id bigint REFERENCES request ON DELETE CASCADE NOT NULL,
     bugfix_commit_hash text NOT NULL,
     repository_url text NOT NULL,
-    szz_variant text NOT NULL,
+    szz_variant text REFERENCES szz_variant NOT NULL,
     request_status req_status NOT NULL DEFAULT 'WAITING'
-);
-
-CREATE TABLE public.szz_variant (
-    id integer NOT NULL,
-    variant_name character varying(20)
 );
 
 ALTER TABLE ONLY public.request ALTER COLUMN request_id SET DEFAULT nextval('public.request_id_seq'::regclass);
@@ -55,9 +55,5 @@ ALTER TABLE public.commit_to_request_link OWNER TO example;
 ALTER TABLE public.szz_variant OWNER TO example;
 
 -- ################ User permissions End #################
-
-
-ALTER TABLE ONLY public.szz_variant
-    ADD CONSTRAINT szz_variant_pkey PRIMARY KEY (id);
 
 GRANT ALL ON SCHEMA public TO example;
