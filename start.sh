4c408b93406cc08a248a739870b0747579d335c1
Felipe Cardoso <rockett370@gmail.com>
2024-10-24 18:05:12 -0300

    feat: Add helper service to populate szz_variant table and update start script

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@ cd $(dirname "$0")
 
 docker compose build
 docker compose up -d git_data_db rabbitmq
-sleep 30
+sleep 10
+docker compose run -e PGPASSWORD=example db_populator psql -U example -h git_data_db -p 5432 -d git_repository -f /app/insert_variants.sql
+sleep 10
 docker compose up -d szz_runner szz_webserver
